5a61dc121560af2f811440b9a6e291e5506fef35
Chester Gardner <Chester.Gardner@academytrainee.com>
2018-12-11 16:20:33 +0000

    Completed Search
    Finished the search inputs for the PHP website - backup
    Next: Loop through the returned results, if found, open it, if not found, look at next page and loop through. Repeat until result or EoPages

--- ATEB_PHPTravels/src/test/java/GroupID/ATEB_PHPTravels/BookHotel.java
+++ ATEB_PHPTravels/src/test/java/GroupID/ATEB_PHPTravels/BookHotel.java
@@ -8,9 +8,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class BookHotel {
 	WebDriver driver;
@@ -44,34 +46,53 @@ public class BookHotel {
 		System.out.println("@Test:seleniumTest");
 		driver.manage().window().maximize();
 		driver.get("https://www.phptravels.net/m-hotels");
+//		driver.get("https://www.phptravels.net/hotels/search/united-kingdom/london/11-12-2018/13-12-2018/3/0");
 
+		Actions action = new Actions(driver);
+		
 		listWebElement = driver.findElements(By.tagName("div"));
 		for (WebElement webele : listWebElement) {
 			try {
 				webele = webele.findElement(By.tagName("input"));
-				webele.sendKeys("London, United Kingdom");
-				System.out.println("found");
+				webele.sendKeys("London");
+				System.out.println("found1");
+				Thread.sleep(1000);
+				action.sendKeys(Keys.TAB).perform();
+				Thread.sleep(1000);
+				action.sendKeys("11/12/2018").perform();
+				System.out.println("found2");
+				Thread.sleep(1000);
+				action.sendKeys(Keys.TAB).perform();
+				Thread.sleep(1000);
+				action.sendKeys("13/12/2018").perform();
+				System.out.println("found3");
 				break;
 			} catch (Exception e) {
 
 			}
 		}
+
 		
-		listWebElement = driver.findElements(By.tagName("input"));
+		webElement = driver.findElement(By.id("travellersInput"));
+		webElement.click();
+		webElement = driver.findElement(By.id("adultPlusBtn"));
+		webElement.click();
+		Thread.sleep(2000);
+		webElement.submit();
+		Thread.sleep(2000);
+
+		int i = 1;
+		listWebElement = driver.findElements(By.className("row"));
 		for (WebElement webele : listWebElement) {
 			try {
-				String placeHolder = webele.getAttribute("placeholder");
-				System.out.println(placeHolder);
-				if (placeHolder == "Check in") {
-					System.out.println("found2");
-					webele.click();	
-				}
+				webele = webele.findElement(By.tagName("a"));
+//				webele.getAttribute("title");
+				System.out.println(Integer.toString(i)+": " + webele.getAttribute("title"));
+				i++;
 			} catch (Exception e) {
 				
 			}
 		}
-		
-
 		Thread.sleep(2000);
 	}
 }
