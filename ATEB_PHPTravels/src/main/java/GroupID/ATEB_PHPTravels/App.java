7dc06f82bf019c647f590a041d98a4322c8246c9
Chester Gardner <Chester.Gardner@academytrainee.com>
2018-12-11 12:28:39 +0000

    First PHPTravels
    Blank project ready to implement a booking of a hotel on a PHP website

---
+++ ATEB_PHPTravels/src/main/java/GroupID/ATEB_PHPTravels/App.java
@@ -0,0 +1,13 @@
+package GroupID.ATEB_PHPTravels;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
